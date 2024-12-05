2376f811acfdbf1f1689f90352b7de321e2681ac
hardened-desktop <157814570+hardened-desktop@users.noreply.github.com>
2024-12-05 17:41:19 +0100

    Update hardened_openbsd_desktop.sh

--- hardened_openbsd_desktop.sh
+++ hardened_openbsd_desktop.sh
@@ -1,11 +1,15 @@
 #!/bin/ksh
 # main idempotent script version at /root/ on "p", last update 2024 dec 05
-# install without the games set "-g*" and without single cpu bsd set "-bsd"
+# dd the newest stable OpenBSD IMG install to a USB flashdrive
+# install fully OFFLINE, no network (is the set partition mounted? "no")
+# and install without the games set "-g*" and without the single cpu bsd set "-bsd"
 # and FDE (full disc encryption) on an SSD
 
+################################
 # check for internet access
 	# TODO
 
+################################
 # install basic softwares
 /usr/sbin/pkg_add firefox \
 	libreoffice \
@@ -14,16 +18,22 @@
 	sshfs-fuse \
 	jhead
 
+################################
 # enable GNOME, info from: /usr/local/share/doc/pkg-readmes/gnome
 	/usr/sbin/rcctl disable xenodm
 	/usr/sbin/rcctl enable multicast messagebus avahi_daemon gdm
 
+################################
 # update erratas: https://www.openbsd.org/errata.html
 	/usr/sbin/syspatch
 
+################################
 # strict malloc: https://man.openbsd.org/free.3#S
 	/usr/bin/sed -i '/vm.malloc_conf/d' /etc/sysctl.conf && \
 	echo 'vm.malloc_conf=S' >> /etc/sysctl.conf
 
+################################
 # sync to disk
 	/bin/sync
+
+################################
